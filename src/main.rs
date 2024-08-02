eed4f72c982a9ccd678549eebeb689263b78608b
Logic-Koike <koike@logic-biz.co.jp>
2024-08-02 17:46:53 +0900

    matchつよい

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,53 @@ fn main() {
     let m = Message::Write(String::from("hello, enum!"));
     m.call();
 
+    // Option<T> : 有効な値か、無効な値を表すenum
+    let suspect_number : Option<i32> = None;
+    let suspect_number2 : Option<i32> = Some(999); // Some(T_value)のような形式で設定する必要がある
+    // let suspect_number3 : i32 = None; // i32にOption<T>::Noneは当然代入できない
+
+    print_number(suspect_number);
+    print_number(suspect_number2);
+
+    // Option<i32>とi32を足すことはできず、コンパイルエラー
+    // i32は必ず有効な値であることがコンパイラで保証されている！いいね
+    // let sum = suspect_number + 30;
+
+    let suspect_number4 = Some(100);
+
+    print_number(suspect_number4);
+    let suspect_number5 = add_number(suspect_number4, 30);
+    print_number(suspect_number5);
+   
+
     println!("Done!");
+}
+
+fn print_number(suspect_number:Option<i32>) {
+match suspect_number {
+        Some(value) => {
+            println!("Value is valid! : {}", value);
+        },
+        None => {
+            // ここでi32にアクセスすることはできない！よい思想ですね
+            println!("Value is None!");
+        }
+    }
+}
+
+fn add_number(suspect_number:Option<i32>,add_number:i32) -> Option<i32> {
+    // match suspect_number {
+    //     Some(value) => {
+    //         return Some(value + add_number);
+    //     },
+    //     None => None,
+    // }
+
+    // 1行で書くこともできます
+    match suspect_number {
+        Some(value) => Some(value + add_number),
+        None => None,
+    }
 }
 
 #[derive(Debug)]
