a5cd16a2797b151ac3a0ee12865d48f428c66fa5
Logic-Koike <koike@logic-biz.co.jp>
2024-08-02 18:23:36 +0900

    struct / enum へのpub付与

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -25,10 +25,47 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast{
+        pub toast: String,
+        seasonal_fluit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fluit: String::from("peaches"),
+            }
+        }
+    }
+
+    // 列挙体は型にpubを付与しておけば、値まるごと公開できる
+    // 一部のみ非公開にすることはできない
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 }
 
 
 pub fn eat_at_restaurant() {
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // 構造体を初期化する関連関数がないと、pubではないフィールドを初期化する手段はない
+    // let meal_private = back_of_house::Breakfast {
+    //     toast: String::from("wheat"),
+    //     seasonal_fluit: String::from("blueberries"),
+    // };
+
+    // 季節のフルーツを知ることも、変更することもできない
+    // meal.seasonal_fluit = String::from("blueberries");
+
+
     // Absolute path
     // crate でクレートルートからスタート
     crate::front_of_house::hosting::add_to_waitlist();
