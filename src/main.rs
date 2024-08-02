7263601211149d5a8819cb0475e9ba78c68c0fb9
Logic-Koike <koike@logic-biz.co.jp>
2024-08-02 12:00:12 +0900

    構造体便利初期化

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,27 @@ fn main() {
     let user = build_user("John".to_string(), "test@test.com".to_string(), 27);
 
     println!("User: {}", user.username);
+    println!("User: {}", user.email);
+
+    // 別のユーザーをインスタンスから生成
+    // let user2 = User {
+    //     username: "Jane".to_string(),
+    //     email: user.email,
+    //     age: user.age,
+    // };
+
+    // ..userでuserの残りのフィールドを使うことができる　便利すなあ
+    let user2 = User {
+        username: "Jane".to_string(),
+        ..user
+    };
+
+    println!("User2: {}", user2.username);
+    println!("User2: {}", user2.email);
 }
 
 fn build_user(username: String, email: String, age: u8) -> User {
+    // フィールド名と同一の変数を使うと省略記法が使える dartの{this.param,...}みたいな感じ
     return User {
         username,
         email,
@@ -24,7 +42,7 @@ fn build_user(username: String, email: String, age: u8) -> User {
 }
 
 struct User {
-    username: String,
+    username: String, // &strで文字列の参照だけを保持しておけばええやんと思うが、それはダメらしい。構造体自身がこのデータを所有する必要がある
     email: String,
     age: u8,
 }
